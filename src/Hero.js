9de564e5a12d6e4c530b04da3377b9e73c211980
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2020-03-19 18:14:22 -0400

    build hero, unresponsive

---
+++ src/Hero.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+    render() {
+        return (
+            <div>
+                <div className='banner container'>
+                PHIRST
+                </div>
+                <div className='hero-diamond'>
+                    <div className='left-triangle'>
+                        <img alt='map of phillipines' src={require('./images/philipine black.png')}></img>
+                    </div>
+                    <div className='right-triangle'>
+                        <div className='slogan'>
+                        Philippine health initiative research service and training
+                        </div>
+                        <div className='divider'></div>
+                        <ul className='hero-navigation'>
+                            <li>about</li>
+                            <li>initiatives</li>
+                            <li>community</li>
+                            <li>research</li>
+                            <li>contact</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Hero;
---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Hero from './Hero.js'
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <Hero />
+            </div>
+        );
+    }
+}
+
+export default Homepage;
