6ccf5415cb2b4b9f45d8f5042d98f91b44f87dd7
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2020-06-22 17:36:50 -0400

    Updates

---
+++ precache-manifest.b9d8555278af5e50e4e1dbb8fbf78236.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467deb8f3c2daaecdb5c92414e8d786f",
+    "url": "/phirst/index.html"
+  },
+  {
+    "revision": "915ebc5c916be5ee337c",
+    "url": "/phirst/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3193a7bde19a01e9f120",
+    "url": "/phirst/static/css/main.f54448da.chunk.css"
+  },
+  {
+    "revision": "915ebc5c916be5ee337c",
+    "url": "/phirst/static/js/2.b85bcbe0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/phirst/static/js/2.b85bcbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3193a7bde19a01e9f120",
+    "url": "/phirst/static/js/main.dc11156d.chunk.js"
+  },
+  {
+    "revision": "662f4c10ef4960c27eb6",
+    "url": "/phirst/static/js/runtime-main.824e4bf7.js"
+  },
+  {
+    "revision": "de8b24d6f4acd603291a1270d34973c3",
+    "url": "/phirst/static/media/ACBermudez.de8b24d6.png"
+  },
+  {
+    "revision": "e1fa39866889f49761f18588640b2910",
+    "url": "/phirst/static/media/Alexander.e1fa3986.JPG"
+  },
+  {
+    "revision": "1bad44fb260c30d06abd6a223911ae2b",
+    "url": "/phirst/static/media/Don-Operario.1bad44fb.jpg"
+  },
+  {
+    "revision": "0849b68aeb5f72ae88fdf773e0a66eb1",
+    "url": "/phirst/static/media/Guigayoma-square.0849b68a.jpg"
+  },
+  {
+    "revision": "219a35e6446635e86b0f631e22b7a807",
+    "url": "/phirst/static/media/Jennifer-Nazareno.219a35e6.jpg"
+  },
+  {
+    "revision": "e7b0f90848683f38e7019b0455598aa0",
+    "url": "/phirst/static/media/Maylin.e7b0f908.jpg"
+  },
+  {
+    "revision": "7d9e1158464004049c5a0f164fc95cae",
+    "url": "/phirst/static/media/PWC.7d9e1158.png"
+  },
+  {
+    "revision": "392bb9e62f0bb1d8bc40b84dbad3a80b",
+    "url": "/phirst/static/media/Quilantang.392bb9e6.jpg"
+  },
+  {
+    "revision": "8204409cc2b3286e8c5b3a0cc9dc133b",
+    "url": "/phirst/static/media/arjee.8204409c.jpg"
+  },
+  {
+    "revision": "60c8d5d836ce9265a9eed9194ae9c04d",
+    "url": "/phirst/static/media/brown-transparent.60c8d5d8.png"
+  },
+  {
+    "revision": "40f9607a3a8d4aee3b25c055d95ae49a",
+    "url": "/phirst/static/media/business-and-finance.40f9607a.png"
+  },
+  {
+    "revision": "6f4160861ad52d18641259f85ea9f704",
+    "url": "/phirst/static/media/educational.6f416086.png"
+  },
+  {
+    "revision": "aaff58aa881b0a9d0d16d4a78cbebf16",
+    "url": "/phirst/static/media/linkedin-icon.aaff58aa.png"
+  },
+  {
+    "revision": "678b4e5369855a792f9f87ba836e924c",
+    "url": "/phirst/static/media/mail-icon.678b4e53.png"
+  },
+  {
+    "revision": "73ca19ed1060aa6b0d3381974886ef38",
+    "url": "/phirst/static/media/maps-and-flags.73ca19ed.png"
+  },
+  {
+    "revision": "a494ce8265d19b6935f6c5f6748034f4",
+    "url": "/phirst/static/media/mentoring.a494ce82.PNG"
+  },
+  {
+    "revision": "c53ebe27823ad283d84b319352a9fd28",
+    "url": "/phirst/static/media/olivia.c53ebe27.png"
+  },
+  {
+    "revision": "b5bc00f18e58cc7e5c08aad40bb4a489",
+    "url": "/phirst/static/media/philipine black.b5bc00f1.png"
+  },
+  {
+    "revision": "2951e66d723326a8fcad6b5bd5bea0ba",
+    "url": "/phirst/static/media/pinoy-plus.2951e66d.jpg"
+  },
+  {
+    "revision": "11dcb11e7bbbbf707067363640f932bb",
+    "url": "/phirst/static/media/red-whistle.11dcb11e.png"
+  },
+  {
+    "revision": "8cbd2ded0d3ec3e0fd3a67c34bcc7f2c",
+    "url": "/phirst/static/media/team.8cbd2ded.jpeg"
+  }
+]);
